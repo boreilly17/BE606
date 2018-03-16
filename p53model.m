aedbf7b0736b79f952037986ac9aa9443dbe2a24
Colin Kunze <cjkunze1@gmail.com>
2018-03-16 13:21:48 -0400

    Updated d_p53p2. Updated d_mdm2p.

--- p53model.m
+++ p53model.m
@@ -1,4 +1,4 @@
-function [CancerElementConcentrations] = CancerModel(t, x, c)
+function [CancerElementConcentrations] = p53model(t, x, c)
 
 p53g = x(1);
 p53m = x(2);
@@ -16,16 +16,17 @@ p53p2u = x(13);
 
 d_p53g = 0;
 d_p53m = (c(1) * p53g) - (c(5) * p53m);
-d_p53p2 = (c(2) * p53g) + (2*c(4) * p53p4) - (c(3) * p53p2 * p53p2) - ...
+d_p53p2 = (c(2) * p53m) + (2*c(4) * p53p4) - (c(3) * p53p2 * p53p2) - ...
     (c(6) * p53p2) - (c(29) * mdm2p * p53p2) + (c(30) * mdm2p_p53p2) + ...
-    (c(47) * p53p2);
-d_p53p4 = (c(3) * p53p2 * p53p2) - (c(7) * p53p4);
+    (c(47) * p53p2) - (c(35) * mdm2p_mdm2p * p53p2) - ...
+    (c(36) * mdm2p_mdm2p_p53p2);
+d_p53p4 = (c(3) * p53p2 * p53p2) - (c(7) * p53p4) - (c(4) * p53p4);
 d_mdm2ga = (c(11) * mdm2gb) - (c(10) * mdm2ga);
 d_mdm2gb = (c(10) * mdm2ga) - (c(11) * mdm2gb);
 % Note: following equation has delay1 on first term
 d_mdm2ma = (c(8) * mdm2ga) - (c(14) * mdm2ma);
-d_mdm2p = (c(9) * mdm2ma) - (c(16) * mdm2p) - (c(18) * mdm2p * mdm2p) + ...
-    (c(17) * mdm2p_mdm2p) - (c(29) * mdm2p * p53p2) + ...
+d_mdm2p = (c(9) * mdm2ma) - (c(16) * mdm2p) + 2*(c(18) * mdm2p_mdm2p) - ...
+    (c(17) * mdm2p * mdm2p) - (c(29) * mdm2p * p53p2) + ...
     (c(30) *mdm2p_p53p2) + (c(13) * mdm2mb);
 %Note: following equation has delay2 on first term
 d_mdm2mb = (c(12) * mdm2gb) - (c(15) * mdm2mb);
@@ -39,5 +40,20 @@ d_mdm2p_mdm2p_p53p2 = (c(31) * mdm2p_p53p2 * mdm2p) - ...
     (c(36) * mdm2p_mdm2p_p53p2) - (c(45) * mdm2p_mdm2p_p53p2);
 d_p53p2u = (c(45) * mdm2p_mdm2p_p53p2) - (c(47) * p53p2u) - ...
     (c(48) * p53p2u);
+
+CancerElementConcentrations = [d_p53g;
+                                d_p53m;
+                                d_p53p2;
+                                d_p53p4;
+                                d_mdm2ga;
+                                d_mdm2gb;
+                                d_mdm2ma;
+                                d_mdm2p;
+                                d_mdm2mb;
+                                d_mdm2p_mdm2p;
+                                d_mdm2p_p53p2;
+                                d_mdm2p_mdm2p_p53p2;
+                                d_p53p2u;
+                                ];
 
 end
